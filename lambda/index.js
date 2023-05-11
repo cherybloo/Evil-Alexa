b4549705f77b909e5e8ef2dca7713c9579b5734c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-11 21:22:51 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -26,6 +26,7 @@ const HelloWorldIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput) {
+        var userInput=handlerInput.requestEnvelope.request.intent.slots.action.value;
         var alexaOutput;
         fetch('https://cherybloo.github.io/suicidal-jokes-api/suicidal.json')
             .then(res=>res.json())
