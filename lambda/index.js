85e3f9621e6a257d5164e7711d028c96e4a00265
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-12 16:00:12 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -67,7 +67,7 @@ const ShitIntentHandler = {
         const musicLink = 'https://cherybloo.github.io/musically/numero-uno.mp3';
         
         return handlerInput.responseBuilder
-            .speak(speakOutput)
+            //.speak(speakOutput)
             .addAudioPlayerPlayDirective(
                 playBehavior,
                 musicLink,
