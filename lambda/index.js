41fb339e28195e8c39a74abb06930347c48bc05b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-14 18:30:27 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -72,9 +72,16 @@ const AnswerIntentHandler = {
     },
     handle(handlerInput) {
         var userAnswer = handlerInput.requestEnvelope.request.intent.slots.Query.value;
+        var anothaAnswer = handlerInput.requestEnvelope.request.intent;
         if(userAnswer.toLowerCase()==alexaAnswer){
             return handlerInput.responseBuilder
                 .speak("yaehya")
+                .reprompt()
+                .getResponse()
+        }
+        else if(anothaAnswer){
+            return handlerInput.responseBuilder
+                .speak("wrong u dumb duck")
                 .reprompt()
                 .getResponse()
         }
