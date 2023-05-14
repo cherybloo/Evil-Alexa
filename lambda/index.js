4b356a45e564416a4365ba8f8952d4f41451433a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-05-14 18:06:05 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -36,9 +36,7 @@ const HelloWorldIntentHandler = {
         await fetch('https://cherybloo.github.io/suicidal-jokes-api/suicidal.json')
             .then(res=>res.json())
             .then(out=>{
-                //var jembut = out[Math.floor(Math.random()*Object.keys(out).length)]
-                var jembut = out[22];
-                //console.log(jembut)
+                var jembut = out[Math.floor(Math.random()*Object.keys(out).length)]
                 if(Object.keys(jembut).length>1){
                     //console.log(jembut['questions']+jembut['answer']);
                     alexaOutput=jembut['questions'];
