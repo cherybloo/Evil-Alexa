93f42e394dfdccdfaf4d76d22a40ecaf5bc4cb42
cherybloo <laurenciomichael98@gmail.com>
2023-06-27 10:32:37 +0200

    All scraping and data cleaning code files

---
+++ scraping and cleaning/dudu.js
@@ -0,0 +1,8 @@
+function onShit(){
+    const response = fetch("https://0ee3-2003-6-13f3-9050-84e3-e226-2749-17a7.ngrok-free.app/2/on")
+}
+
+function offShit(){
+    const response = fetch("https://0ee3-2003-6-13f3-9050-84e3-e226-2749-17a7.ngrok-free.app/2/off")
+}
+
